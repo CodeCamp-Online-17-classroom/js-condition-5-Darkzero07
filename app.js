7b27f4b3335099284874630b2019cdb7da477c08
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 04:53:39 +0700

    js lab 22

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+let num = Number(prompt("Input your number : "));
+if (num == 0 ) {
+    alert("Zero");
+}else if (num > 0) {
+    alert("Positive number");
+}else if (num < 0) {
+    alert("Negative number");
+}else {
+    alert("Invalid number")
+}
